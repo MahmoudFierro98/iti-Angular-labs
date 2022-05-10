98e8163f85c84d0ddf40594664bf0500aa7ccabb
Mahmoud Mohamed Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2022-05-10 20:21:20 +0200

    Add Lab 3

---
+++ Lab3/Lab3/src/app/department/department/department.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/_models/department';
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './department.component.html',
+  styleUrls: ['./department.component.css']
+})
+export class DepartmentComponent implements OnInit {
+  departments: Department[] = [
+    new Department(1, "OS", "Alex"),
+    new Department(2, "WEB", "Alex"),
+    new Department(3, "Mobile", "Alex")];
+
+  newDepartment: Department = new Department(0, "", "");
+  viewDepartment: Department = new Department(0, "hidden0000", "");
+  department: Department = new Department(0, "hidden0000", "");
+
+  showFlag = false;
+  editFlag = false;
+
+  addNewDepartment() {
+    this.showFlag = false;
+    this.editFlag = false;
+    this.departments.push(new Department(this.newDepartment.id, this.newDepartment.deptname, this.newDepartment.location));
+    this.newDepartment.id = 0;
+    this.newDepartment.deptname = "";
+    this.newDepartment.location = "";
+  }
+
+  showDepartment(departmentId: number) {
+    this.showFlag = true;
+    for (let i = 0; i < this.departments.length; i++) {
+      if (this.departments[i].id == departmentId) {
+        this.viewDepartment = this.departments[i];
+        break;
+      }
+    }
+  }
+
+  editDepartment(departmentId: number) {
+    this.editFlag = true;
+    for (let i = 0; i < this.departments.length; i++) {
+      if (this.departments[i].id == departmentId) {
+        this.department = this.departments[i];
+        break;
+      }
+    }
+  }
+
+  deleteDepartment(departmentId: number) {
+    this.showFlag = false;
+    this.editFlag = false;
+    for (let i = 0; i < this.departments.length; i++) {
+      if (this.departments[i].id == departmentId) {
+        this.departments.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
